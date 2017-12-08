3af74b7c4d198a48419e8a3d64cc7072a245a8bd
davidmarsland <david.marsland@gmail.com>
2017-12-08 14:11:10 -0800

    Removed bitmaker after git security warnings on dependencies

--- my-project-bitmaker/src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('myProjectBitmaker', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'restangular', 'ui.router', 'ui.bootstrap', 'toastr']);
-
-})();
